bccb2649e7c68d5a70af180cfaa577bda4781e8f
Sayani637 <paulsayani304@gmail.com>
2023-03-06 21:15:41 +0530

    Connection successfull msg

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 if(isset($_GET['deleteid'])){
     $id = $_GET['deleteid'];
-    print_r( $id);
+    // print_r( $id);
     $sql = "delete from `crud` where id=$id" ;
     $result = mysqli_query($conn,$sql);
     if($result){
